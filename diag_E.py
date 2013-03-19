93013c98c47a22855e3e283ce627d3bf43f95120
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-03-19 16:01:32 +0100

    - added script for energy and helicity plots

---
+++ diag_E.py
@@ -0,0 +1,167 @@
+'''
+Created on Apr 06, 2012
+
+@author: Michael Kraus (michael.kraus@ipp.mpg.de)
+'''
+
+import argparse
+
+import numpy as np
+
+import matplotlib
+#matplotlib.use('Cairo')
+matplotlib.use('AGG')
+#matplotlib.use('PDF')
+
+import matplotlib.pyplot as plt
+from matplotlib import cm, colors, gridspec
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter, ScalarFormatter
+
+
+from diagnostics import Diagnostics 
+
+
+class PlotEnergy(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, diagnostics, filename, ntMax=0, nPlot=1):
+        '''
+        Constructor
+        '''
+        
+        matplotlib.rc('text', usetex=True)
+        matplotlib.rc('font', family='sans-serif', size='24')
+        
+        self.prefix = filename
+        
+        self.ntMax = diagnostics.nt
+        
+        if self.ntMax > ntMax and ntMax > 0:
+            self.ntMax = ntMax
+        
+        self.nPlot = nPlot
+        
+        self.diagnostics = diagnostics
+        
+        
+        self.energy      = np.zeros(self.ntMax+1)
+        self.helicity    = np.zeros(self.ntMax+1)
+        
+        
+        print("")
+        for i in range(0, self.ntMax+1):
+            print("Reading timestep %5i" % (i))
+            
+            self.diagnostics.read_from_hdf5(i)
+            self.diagnostics.update_invariants(i)
+            
+            if self.diagnostics.plot_energy:
+                self.energy  [i] = self.diagnostics.energy
+            else:
+                self.energy  [i] = self.diagnostics.E_error
+            
+            if self.diagnostics.plot_helicity:
+                self.helicity[i] = self.diagnostics.helicity
+            else:
+                self.helicity[i] = self.diagnostics.H_error
+            
+        
+        # set up tick formatter
+        majorFormatter = ScalarFormatter(useOffset=False)
+        ## -> limit to 1.1f precision
+        majorFormatter.set_powerlimits((-1,+1))
+        majorFormatter.set_scientific(True)
+
+
+        # set up figure for energy plot
+        self.figure1 = plt.figure(num=1, figsize=(16,5))
+        
+        # set up plot margins
+        plt.subplots_adjust(hspace=0.25, wspace=0.2)
+        plt.subplots_adjust(left=0.1, right=0.95, top=0.93, bottom=0.15)
+        
+        axesE = plt.subplot(1,1,1)
+        axesE.plot(self.diagnostics.tGrid[0:ntMax+1:self.nPlot], self.energy[0:ntMax+1:self.nPlot])
+        
+        axesE.set_xlabel('$t$', labelpad=15, fontsize=24)
+        
+        if self.diagnostics.plot_energy:
+            axesE.set_ylabel('$E (t)$', labelpad=15, fontsize=24)
+        else:
+            axesE.set_ylabel('$(E-E_0) / E_0 (t)$', labelpad=15, fontsize=24)
+        
+        axesE.yaxis.set_major_formatter(majorFormatter)
+        
+        plt.draw()
+        
+        filename = self.prefix + str('_energy_%06d' % self.ntMax) + '.png'
+        plt.savefig(filename, dpi=300)
+        filename = self.prefix + str('_energy_%06d' % self.ntMax) + '.pdf'
+        plt.savefig(filename)
+        
+        
+        # set up figure for helicity plot
+        self.figure2 = plt.figure(num=2, figsize=(16,5))
+        
+        # set up plot margins
+        plt.subplots_adjust(hspace=0.25, wspace=0.2)
+        plt.subplots_adjust(left=0.1, right=0.95, top=0.93, bottom=0.15)
+        
+        axesH = plt.subplot(1,1,1)
+        axesH.plot(self.diagnostics.tGrid[0:ntMax+1:self.nPlot], self.helicity[0:ntMax+1:self.nPlot])
+        
+        axesH.set_xlabel('$t$', labelpad=15, fontsize=24)
+        
+        if self.diagnostics.plot_helicity:
+            axesH.set_ylabel('$H (t)$', labelpad=15, fontsize=24)
+        else:
+            axesH.set_ylabel('$(H-H_0) / H_0 (t)$', labelpad=15, fontsize=24)
+        
+        axesH.yaxis.set_major_formatter(majorFormatter)
+        
+        plt.draw()
+        
+        filename = self.prefix + str('_helicity_%06d' % self.ntMax) + '.png'
+        plt.savefig(filename, dpi=300)
+        filename = self.prefix + str('_helicity_%06d' % self.ntMax) + '.pdf'
+        plt.savefig(filename)
+        
+        
+
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vlasov-Poisson Solver in 1D')
+    
+    parser.add_argument('hdf5_file', metavar='<run.hdf5>', type=str,
+                        help='Run HDF5 File')
+    parser.add_argument('-np', metavar='i', type=int, default=1,
+                        help='plot every i\'th frame')
+    parser.add_argument('-ntmax', metavar='i', type=int, default=0,
+                        help='limit to i points in time')
+    
+    args = parser.parse_args()
+    
+    
+    print
+    print("Replay run with " + args.hdf5_file)
+    print
+    
+    diagnostics = Diagnostics(args.hdf5_file)
+    
+    ntMax=args.ntmax
+    nPlot=args.np
+    
+    if ntMax > 0 and ntMax < diagnostics.nt:
+        nt = ntMax
+    else:
+        nt = diagnostics.nt
+    
+    plot  = PlotEnergy(diagnostics, args.hdf5_file.replace(".hdf5", ""), nt, nPlot)
+    
+    print
+    print("Replay finished.")
+    print
+    
