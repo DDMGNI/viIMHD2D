3c4783fc89ce3d2f38273abc3c1956b803291b15
Michael Kraus <michael.kraus@ipp.mpg.de>
2015-11-26 11:09:23 +0100

    Added new imhd package as starting point for restructuring the code.

---
+++ imhd/integrators/Inertial_MHD_NL_Function.pyx
@@ -0,0 +1,273 @@
+'''
+Created on Apr 10, 2012
+
+@author: mkraus
+'''
+
+cimport cython
+
+import  numpy as np
+cimport numpy as np
+
+from petsc4py import  PETSc
+from petsc4py cimport PETSc
+
+from petsc4py.PETSc cimport SNES, Mat, Vec
+
+from PETSc_MHD_Derivatives import  PETSc_MHD_Derivatives
+from PETSc_MHD_Derivatives cimport PETSc_MHD_Derivatives
+
+
+
+cdef class PETScFunction(object):
+    '''
+    
+    '''
+    
+    def __init__(self, object da1, object da5,
+                 int nx, int ny,
+                 double ht, double hx, double hy,
+                 double mu, double nu, double eta, double de):
+        '''
+        Constructor
+        '''
+        
+        # distributed array
+        self.da1 = da1
+        self.da5 = da5
+        
+        # grid
+        self.nx = nx
+        self.ny = ny
+        
+        self.ht = ht
+        self.hx = hx
+        self.hy = hy
+        
+        self.ht_inv = 1. / self.ht
+        
+        # friction, viscosity, resistivity, electron skin depth
+        self.mu  = mu
+        self.nu  = nu
+        self.eta = eta
+        self.de  = de
+        
+        # create history vector
+        self.Xh = self.da5.createGlobalVec()
+        
+        # create local vectors
+        self.localX  = da5.createLocalVec()
+        self.localXh = da5.createLocalVec()
+        
+        # create derivatives object
+        self.derivatives = PETSc_MHD_Derivatives(nx, ny, ht, hx, hy)
+        
+    
+    def update_history(self, Vec X):
+        X.copy(self.Xh)
+        
+    
+    def mult(self, Mat mat, Vec X, Vec Y):
+        self.matrix_mult(X, Y)
+        
+    def snes_mult(self, SNES snes, Vec X, Vec Y):
+        self.matrix_mult(X, Y)
+        
+    
+    @cython.boundscheck(False)
+    def matrix_mult(self, Vec X, Vec Y):
+        cdef int i, j
+        cdef int ix, iy, jx, jy
+        cdef int xe, xs, ye, ys
+        
+        (xs, xe), (ys, ye) = self.da1.getRanges()
+        
+        self.da5.globalToLocal(X,       self.localX)
+        self.da5.globalToLocal(self.Xh, self.localXh)
+        
+        cdef np.ndarray[double, ndim=3] x  = self.da5.getVecArray(self.localX) [...]
+        cdef np.ndarray[double, ndim=3] xh = self.da5.getVecArray(self.localXh)[...]
+        
+        cdef np.ndarray[double, ndim=3] y = self.da5.getVecArray(Y)[...]
+
+        cdef np.ndarray[double, ndim=2] Vx  =  x [:,:,0]
+        cdef np.ndarray[double, ndim=2] Vy   = x [:,:,1]
+        cdef np.ndarray[double, ndim=2] Bx   = x [:,:,2]
+        cdef np.ndarray[double, ndim=2] By   = x [:,:,3]
+        cdef np.ndarray[double, ndim=2] Bix  = x [:,:,4]
+        cdef np.ndarray[double, ndim=2] Biy  = x [:,:,5]
+        cdef np.ndarray[double, ndim=2] P    = x [:,:,6]
+        
+        cdef np.ndarray[double, ndim=2] Vxh  = xh[:,:,0]
+        cdef np.ndarray[double, ndim=2] Vyh  = xh[:,:,1]
+        cdef np.ndarray[double, ndim=2] Bxh  = xh[:,:,2]
+        cdef np.ndarray[double, ndim=2] Byh  = xh[:,:,3]
+        cdef np.ndarray[double, ndim=2] Bixh = xh[:,:,4]
+        cdef np.ndarray[double, ndim=2] Biyh = xh[:,:,5]
+        cdef np.ndarray[double, ndim=2] Ph   = xh[:,:,6]
+        
+        cdef double[:,:] Vx_ave  = 0.5 * (Vx  + Vxh )
+        cdef double[:,:] Vy_ave  = 0.5 * (Vy  + Vyh )
+        cdef double[:,:] Bx_ave  = 0.5 * (Bx  + Bxh )
+        cdef double[:,:] By_ave  = 0.5 * (By  + Byh )
+        cdef double[:,:] Bix_ave = 0.5 * (Bix + Bixh)
+        cdef double[:,:] Biy_ave = 0.5 * (Biy + Biyh)
+        cdef double[:,:] P_ave   = 0.5 * (P   + Ph  )
+
+        
+        for i in range(xs, xe):
+            ix = i-xs+2
+            iy = i-xs
+            
+            for j in range(ys, ye):
+                jx = j-ys+2
+                jy = j-ys
+                
+                # V_x
+                y[iy, jy, 0] = self.dt(Vx,  ix, jx) \
+                             - self.dt(Vxh, ix, jx) \
+                             + self.derivatives.psix(Vx_ave,  Vy_ave,  Vx_ave, Vy_ave, ix, jx) \
+                             - self.derivatives.psix(Bix_ave, Biy_ave, Bx_ave, By_ave, ix, jx) \
+                             + self.derivatives.divx_sg(P,  ix, jx) \
+                             + 0.5 * self.mu * Vx [ix,jx] \
+                             + 0.5 * self.mu * Vxh[ix,jx]
+                
+                # V_y
+                y[iy, jy, 1] = self.dt(Vy,  ix, jx) \
+                             - self.dt(Vyh, ix, jx) \
+                             + self.derivatives.psiy(Vx_ave,  Vy_ave,  Vx_ave, Vy_ave, ix, jx) \
+                             - self.derivatives.psiy(Bix_ave, Biy_ave, Bx_ave, By_ave, ix, jx) \
+                             + self.derivatives.divy_sg(P,  ix, jx) \
+                             + 0.5 * self.mu * Vy [ix,jx] \
+                             + 0.5 * self.mu * Vyh[ix,jx]
+                
+                # B_x
+                y[iy, jy, 2] = Bix[ix, jx] \
+                             - self.derivatives.Bix(Bx, By, ix, jx, self.de)
+                
+                # B_y
+                y[iy, jy, 3] = Biy[ix, jx] \
+                             - self.derivatives.Biy(Bx, By, ix, jx, self.de)
+                
+                # Bi_x
+                y[iy, jy, 4] = self.dt(Bix,  ix, jx) \
+                             - self.dt(Bixh, ix, jx) \
+                             + self.derivatives.phix(Vx_ave,  Biy_ave, ix, jx) \
+                             - self.derivatives.phix(Bix_ave, Vy_ave,  ix, jx)
+                
+                # Bi_y
+                y[iy, jy, 5] = self.dt(Biy,  ix, jx) \
+                             - self.dt(Biyh, ix, jx) \
+                             + self.derivatives.phiy(Vx_ave,  Biy_ave, ix, jx) \
+                             - self.derivatives.phiy(Bix_ave, Vy_ave,  ix, jx)
+                
+                # P
+                y[iy, jy, 6] = self.derivatives.gradx_simple(Vx_ave, ix, jx) \
+                             + self.derivatives.grady_simple(Vy_ave, ix, jx)
+                
+
+
+    @cython.boundscheck(False)
+    def timestep(self, np.ndarray[double, ndim=3] x,
+                       np.ndarray[double, ndim=3] y):
+        
+        cdef int i, j
+        cdef int ix, iy, jx, jy
+        cdef int xe, xs, ye, ys
+        
+        (xs, xe), (ys, ye) = self.da1.getRanges()
+        
+        cdef double[:,:] Vx  = x[:,:,0]
+        cdef double[:,:] Vy  = x[:,:,1]
+        cdef double[:,:] Bx  = x[:,:,2]
+        cdef double[:,:] By  = x[:,:,3]
+        cdef double[:,:] Bix = x[:,:,4]
+        cdef double[:,:] Biy = x[:,:,5]
+        cdef double[:,:] P   = x[:,:,6]
+
+        
+        for i in range(xs, xe):
+            ix = i-xs+2
+            iy = i-xs
+            
+            for j in range(ys, ye):
+                jx = j-ys+2
+                jy = j-ys
+                
+                # V_x
+                y[iy, jy, 0] = \
+                             + self.derivatives.psix(Vx,  Vy,  Vx, Vy, ix, jx) \
+                             - self.derivatives.psix(Bix, Biy, Bx, By, ix, jx) \
+                             + self.derivatives.divx_sg(P, ix, jx)
+                
+                # V_y
+                y[iy, jy, 1] = \
+                             + self.derivatives.psiy(Vx,  Vy,  Vx, Vy, ix, jx) \
+                             - self.derivatives.psiy(Bix, Biy, Bx, By, ix, jx) \
+                             + self.derivatives.divy_sg(P, ix, jx)
+                              
+                # B_x
+                y[iy, jy, 2] = Bx[ix,jx]
+                
+                # B_y
+                y[iy, jy, 3] = By[ix,jx]
+                
+                # Bi_x
+                y[iy, jy, 4] = \
+                             + self.derivatives.phix(Vx, Biy, ix, jx) \
+                             - self.derivatives.phix(Bix, Vy, ix, jx)
+                    
+                # Bi_y
+                y[iy, jy, 5] = \
+                             + self.derivatives.phiy(Vx, Biy, ix, jx) \
+                             - self.derivatives.phiy(Bix, Vy, ix, jx)
+                
+                # P
+                y[iy, jy, 6] = P[ix,jx]
+                             
+        
+
+
+    @cython.boundscheck(False)
+    cdef double dt(self, double[:,:] x, int i, int j):
+        '''
+        Time Derivative
+        '''
+        
+        return x[i, j] * self.ht_inv
+    
+    
+    
+    @cython.boundscheck(False)
+    cdef double dt_x(self, double[:,:] x, int i, int j):
+        '''
+        Time Derivative
+        '''
+        
+        cdef double result
+        
+        result = ( \
+                     + 1. * x[i,   j-1] \
+                     + 2. * x[i,   j  ] \
+                     + 1. * x[i,   j+1] \
+                 ) * 0.25 * self.ht_inv
+                 
+        return result
+    
+    
+    
+    @cython.boundscheck(False)
+    cdef double dt_y(self, double[:,:] x, int i, int j):
+        '''
+        Time Derivative
+        '''
+        
+        cdef double result
+        
+        result = ( \
+                     + 1. * x[i-1, j  ] \
+                     + 2. * x[i,   j  ] \
+                     + 1. * x[i+1, j  ] \
+                 ) * 0.25 * self.ht_inv
+        
+        return result
---
+++ imhd/integrators/MHD_Derivatives.pxd
@@ -0,0 +1,133 @@
+'''
+Created on May 24, 2012
+
+@author: Michael Kraus (michael.kraus@ipp.mpg.de)
+'''
+
+cimport numpy as np
+
+
+cdef class PETSc_MHD_Derivatives(object):
+    '''
+    Cython Implementation of MHD Discretisation
+    '''
+    
+    cdef int  nx
+    cdef int  ny
+    
+    cdef double ht
+    cdef double hx
+    cdef double hy
+    
+    cdef double ht_inv
+    cdef double hx_inv
+    cdef double hy_inv
+    
+    cdef double hx_inv2
+    cdef double hy_inv2
+    
+    
+        
+    cdef double dx(self, double[:,:] B,
+                               double[:,:] V,
+                               int i, int j)
+    
+    cdef double dy(self, double[:,:] B,
+                               double[:,:] V,
+                               int i, int j)
+
+    cdef double fdudx(self, double[:,:] F,
+                                  double[:,:] U,
+                                  int i, int j)
+
+    cdef double fdudy(self, double[:,:] F,
+                                  double[:,:] U,
+                                  int i, int j)
+    
+    cdef double fx_dx_ux(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+    cdef double fx_dx_uy(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+    cdef double fy_dy_ux(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+    cdef double fy_dy_uy(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+    cdef double dx_fx_uy(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+    cdef double dy_fx_uy(self, double[:,:] F,
+                                     double[:,:] U,
+                                     int i, int j)
+    
+
+    cdef double gradx(self, double[:,:] x,
+                                  int i, int j)
+    
+    cdef double grady(self, double[:,:] x,
+                                  int i, int j)
+    
+    cdef double gradx_sg(self, double[:,:] x,
+                                     int i, int j)
+
+    cdef double grady_sg(self, double[:,:] x,
+                                     int i, int j)
+
+    cdef double gradx_fv(self, double[:,:] x,
+                                     int i, int j)
+    
+    cdef double grady_fv(self, double[:,:] x,
+                                     int i, int j)
+
+    cdef double gradx_simple(self, double[:,:] x,
+                                         int i, int j)
+    
+    cdef double grady_simple(self, double[:,:] x,
+                                         int i, int j)
+
+    cdef double divx(self, double[:,:] x,
+                                 int i, int j)
+    
+    cdef double divy(self, double[:,:] x,
+                                 int i, int j)
+
+    cdef double divx_sg(self, double[:,:] x,
+                                    int i, int j)
+
+    cdef double divy_sg(self, double[:,:] x,
+                                    int i, int j)
+
+    cdef double laplace(self, double[:,:] x,
+                                    int i, int j)
+
+    cdef double laplace_fd(self, double[:,:] x,
+                                       int i, int j)
+
+
+    cdef double dt(self, double[:,:] x,
+                               int i, int j)
+    
+    cdef double dt_diag(self, double[:,:] x,
+                                    int i, int j)
+
+
+    cdef double rot(self, double[:,:] Ux, double[:,:] Uy, int i, int j)
+
+    cdef double psix(self, double[:,:] Ux, double[:,:] Uy, double[:,:] Vx, double[:,:] Vy, int i, int j)
+    cdef double psiy(self, double[:,:] Ux, double[:,:] Uy, double[:,:] Vx, double[:,:] Vy, int i, int j)
+
+    cdef double phix(self, double[:,:] F, double[:,:] U, int i, int j)
+    cdef double phiy(self, double[:,:] F, double[:,:] U, int i, int j)
+
+    cpdef double Bix(self, double[:,:] Bx, double[:,:] By, int i, int j, double de)
+    cpdef double Biy(self, double[:,:] Bx, double[:,:] By, int i, int j, double de)
+    
+    
