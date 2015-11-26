7e33e3705df482f14bbe7c953e4b2ec8194ad1db
Michael Kraus <michael.kraus@ipp.mpg.de>
2015-11-26 17:38:20 +0100

    Added PETSc options to config file.

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ from validate  import Validator
 
 import os.path
 
+from petsc4py import PETSc
+
 
 class Config(ConfigObj):
     '''
@@ -36,6 +38,45 @@ class Config(ConfigObj):
         '''
         
         self.write()
+    
+    
+    def set_petsc_options(self):
+        
+        OptDB = PETSc.Options()
+        
+        if self['solver']['petsc_snes_type'] is 'newton_basic':
+            OptDB.setValue('snes_ls', 'basic')
+            
+        elif self['solver']['petsc_snes_type'] is 'newton_quadratic':
+            OptDB.setValue('snes_ls', 'quadratic')
+#         else:
+#             raise Exception("Invalid SNES type.")
+        
+        OptDB.setValue('snes_atol',   self['solver']['petsc_snes_atol'])
+        OptDB.setValue('snes_rtol',   self['solver']['petsc_snes_rtol'])
+        OptDB.setValue('snes_stol',   self['solver']['petsc_snes_stol'])
+        OptDB.setValue('snes_max_it', self['solver']['petsc_snes_max_iter'])
+        OptDB.setValue('ksp_atol',    self['solver']['petsc_ksp_atol'])
+        OptDB.setValue('ksp_rtol',    self['solver']['petsc_ksp_rtol'])
+        OptDB.setValue('ksp_max_it',  self['solver']['petsc_ksp_max_iter'])
+        OptDB.setValue('ksp_type',    self['solver']['petsc_ksp_type'])
+        OptDB.setValue('pc_type',     self['solver']['petsc_pc_type'])
+        
+        if self['solver']['petsc_snes_monitor']:
+            OptDB.setValue('snes_monitor', '')
+        
+        if self['solver']['petsc_ksp_monitor']:
+            OptDB.setValue('ksp_monitor', '')
+        
+        if self['solver']['petsc_pc_type'] is 'lu':
+            OptDB.setValue('pc_factor_solver_package', self['solver']['petsc_lu_package'])
+            
+        elif self['solver']['petsc_pc_type'] is 'asm':
+            OptDB.setValue('pc_asm_type',  'restrict')
+            OptDB.setValue('pc_asm_overlap', 3)
+            OptDB.setValue('sub_ksp_type', 'preonly')
+            OptDB.setValue('sub_pc_type', 'lu')
+            OptDB.setValue('sub_pc_factor_mat_solver_package', self['solver']['petsc_lu_package'])
         
     
 
--- runs/current_sheet_reconnection.py
+++ runs/current_sheet_reconnection.py
@@ -10,14 +10,13 @@ def magnetic_x(x, y, Lx, Ly):
     return 0.0
 
 def magnetic_y(x, y, Lx, Ly):
-#     return - B0 / np.cosh(x)**2
     return - 2*B0 * np.tanh(x) / np.cosh(x)**2    
 
 def velocity_x(x, y, Lx, Ly):
-    return - u0 * ( np.sin(x+y) + np.sin(x-y) )
+    return + u0 * ( np.sin(x+y) + np.sin(x-y) )
 
 def velocity_y(x, y, Lx, Ly):
-    return + u0 * ( np.sin(x+y) - np.sin(x-y) )
+    return - u0 * ( np.sin(x+y) - np.sin(x-y) )
 
 def pressure(x, y, Lx, Ly):
     return p0
